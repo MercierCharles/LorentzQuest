fcaef6b02578f0fb330746a23296638b37842e0c
adriangomezortiz <adrian.gomezortiz@epfl.ch>
2025-04-08 12:02:43 +0200

    Level 6
    La cam ne suit pas l'électron

--- scripts/camera_2d_level6.gd
+++ scripts/camera_2d_level6.gd
@@ -1,10 +1,6 @@
 extends Camera2D
-
 @onready var targetProton: RigidBody2D = $"../ProtonLev2"
-
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if targetProton:
 		position.x = lerp(position.x, targetProton.position.x, 5 * delta)  # Lissage du suivi en X
-			
